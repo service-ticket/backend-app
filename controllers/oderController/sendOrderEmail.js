c8d1720d66026e94570e4bafc60e28736755b0bd
service-ticket <ticket.marcket@gmail.com>
2025-10-16 19:32:30 -0600

    first correo

--- controllers/oderController/sendOrderEmail.js
+++ controllers/oderController/sendOrderEmail.js
@@ -1,10 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // TLS
     auth: {
-        user: process.env.EMAIL_USER, // tu correo
-        pass: process.env.EMAIL_PASS  // tu app password
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
